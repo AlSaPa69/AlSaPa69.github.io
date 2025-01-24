1c852f1ab353a3a342cc160f99aaf8bbfcbccbad
Alejandro Sabater <alesabpas@alu.edu.gva.es>
2025-01-24 20:48:49 +0100

    - Transcribed styles to stylesheets - Edited index.html to work with stylesheets

---
+++ scripts/fetch-nav.js
@@ -0,0 +1,8 @@
+// Este módulo es utilizado para fetchear el contenido de la tabla de navegación.
+const htmlRoute = 'https://alsapa69.github.io/hola.html'; // Ruta para acceder al contenido de la sidebar.
+
+const sidebar = document.getElementsByClassName('sidebar')[0];
+fetch(htmlRoute).then(res => res.text()).then(res => {
+    sidebar.innerHTML = res;
+    console.log(res);
+});
