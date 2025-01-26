ce0b6841599d8e84d468bb828944ae6d6af2830d
Alejandro Sabater <alesabpas@alu.edu.gva.es>
2025-01-26 17:46:39 +0100

    - Deleted baldiold.html - hola.html has been renamed and edited to sidebar_source.html - fetch-nav.js works now with sidebar_source.html

--- scripts/fetch-nav.js
+++ scripts/fetch-nav.js
@@ -1,5 +1,5 @@
 // Este módulo es utilizado para fetchear el contenido de la tabla de navegación.
-const htmlRoute = 'https://alsapa69.github.io/hola.html'; // Ruta para acceder al contenido de la sidebar.
+const htmlRoute = 'https://alsapa69.github.io/sidebar_src.html'; // Ruta para acceder al contenido de la sidebar.
 
 const sidebar = document.getElementsByClassName('sidebar')[0];
 fetch(htmlRoute).then(res => res.text()).then(res => {
