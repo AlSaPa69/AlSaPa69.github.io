c8ab59804312d3ab7710b76064a0461bf458019e
Dhymbot <dhymbot@noreply.github.com>
2025-03-15 23:57:17 +0100

    texto de farmeo

--- basketedited/scripts/c3runtime.js
+++ basketedited/scripts/c3runtime.js
@@ -42174,7 +42174,7 @@
 			const _ = g.g_$K(0).g_$A(),
 				e = g.g_$K(1).g_$c(),
 				a = g.g_$K(2).g_$A();
-			return () => _.g_Pc() + ((0 < e(a.g_Pc()) ? 1 : 0) ? ' + ' : '') + console.log(g) + 'LIGHT';
+			return () => _.g_Pc() + ((0 < e(a.g_Pc()) ? 1 : 0) ? ' + ' : '') + console.log(g.POINTS) + 'LIGHT';
 		},
 		() => 'kames',
 		(g) => {
